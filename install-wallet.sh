cdfe67e0b2c0bd69a60d26e01ac8e2ca782270d5
SalenCoin <36447267+SalenCoin@users.noreply.github.com>
2018-03-02 17:48:50 +0300

    Update install-wallet.sh

--- install-wallet.sh
+++ install-wallet.sh
@@ -14,10 +14,3 @@ sudo add-apt-repository ppa:bitcoin/bitcoin
 sudo apt-get update
 sudo apt-get install libdb4.8-dev libdb4.8++-dev wget -y
 
-sudo ufw allow 12225/tcp
-
-wget https://www.salen.com/release/salencore-latest-linux64.tar.gz
-tar -xvzf salencore-latest-linux64.tar.gz
-cd salencore*/bin
-cp salen-cli salend /bin/
-
